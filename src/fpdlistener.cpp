acd1d090a40290c42a974f395c7482b600e747ef
Bardia Moshiri <fakeshell@bardia.tech>
2024-05-23 05:42:44 -0400

    fpdlistener: unlock and vibrate using logind and feedbackd dbus
    Signed-off-by: Bardia Moshiri <fakeshell@bardia.tech>

--- src/fpdlistener.cpp
+++ src/fpdlistener.cpp
@@ -1,25 +1,33 @@
 // SPDX-License-Identifier: GPL-2.0-only
-// Copyright (C) 2023 Droidian Project
+// Copyright (C) 2024 Droidian Project
 //
 // Authors:
 // Bardia Moshiri <fakeshell@bardia.tech>
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
-#include <sys/wait.h>
 #include <batman/wlrdisplay.h>
 #include <QCoreApplication>
-#include <QTextStream>
 #include <QDebug>
-#include <QProcess>
 #include <QThread>
-#include <QDir>
-#include <QStandardPaths>
-#include <QSharedPointer>
-#include <QFile>
+#include <QDBusInterface>
+#include <QDBusReply>
 #include "fpdinterface.h"
+
+static void unlockSession(const QString &sessionId, int &exitStatus) {
+    QDBusInterface interface("org.freedesktop.login1", "/org/freedesktop/login1", "org.freedesktop.login1.Manager", QDBusConnection::systemBus());
+    if (interface.isValid()) {
+        QDBusReply<void> reply = interface.call("UnlockSession", sessionId);
+        if (reply.isValid()) {
+            exitStatus = 0;
+        } else {
+            qWarning() << "DBus call failed: " << reply.error().message();
+            exitStatus = 1;
+        }
+    } else {
+        qWarning() << "DBus interface is invalid";
+        exitStatus = 1;
+    }
+}
 
 extern "C" int wlrdisplay_status() {
     int result = wlrdisplay(0, NULL);
@@ -30,58 +38,16 @@ extern "C" int delay(double seconds) {
     return usleep(seconds * 1000000);
 }
 
-QString getConfigFile() {
-    QFileInfo primaryConfig("/etc/droidian-fpd-client.conf");
-    QFileInfo secodaryConfig("/usr/share/droidian-fpd-client/droidian-fpd-client.conf");
-
-    if (primaryConfig.exists()) {
-        return primaryConfig.absoluteFilePath();
-    } else if (secodaryConfig.exists()) {
-        return secodaryConfig.absoluteFilePath();
-    } else {
-        return "None";
-    }
-}
-
-QString getVibrationLevel(bool connect) {
-    int vibration_level_selected = 1; // default is 1
-    QString configFilePath = getConfigFile();
-    QString vibraCommand = "";
-    // qDebug() << "config file path: " << configFilePath;
-    QFile configFile(configFilePath);
-
-    if(configFile.open(QIODevice::ReadOnly)) {
-        QTextStream in(&configFile);
-        QString line;
-
-        while (in.readLineInto(&line)) {
-            if (line.startsWith("vibration_level=")) {
-                int vibrationLevel = (line.split("=")[1].trimmed()).toInt();
-                // qDebug() << vibrationLevel;
-                if (vibrationLevel == 0) {
-                    vibration_level_selected = -1;
-                    // return empty string for no vibration
-                    // qDebug() << "vibration off";
-                } else {
-                    // qDebug() << vibrationLevel;
-                    vibration_level_selected = vibrationLevel - 1;
-                }
-
-                break;
-            }
+void sendFeedback(const QString &event) {
+    QDBusInterface feedbackdInterface("org.sigxcpu.Feedback", "/org/sigxcpu/Feedback", "org.sigxcpu.Feedback", QDBusConnection::sessionBus());
+    if (feedbackdInterface.isValid()) {
+        QDBusReply<void> reply = feedbackdInterface.call("TriggerFeedback", "fpdlistener", event, QVariantMap(), -1);
+        if (!reply.isValid()) {
+            qWarning() << "DBus call failed: " << reply.error().message();
         }
+    } else {
+        qWarning() << "DBus interface for feedbackd is invalid";
     }
-
-    if (connect && vibration_level_selected != -1) {
-        // good finger
-        vibraCommand = QStringLiteral("for i in {0..%1}; do fbcli -E button-released; done").arg(vibration_level_selected);
-    } else if (!connect && vibration_level_selected != -1) {
-        // bad finger
-        vibraCommand = QStringLiteral("for i in {0..%1}; do fbcli -E window-close; done").arg(vibration_level_selected);
-    }
-
-    // qDebug() << vibraCommand;
-    return vibraCommand;
 }
 
 void fpdunlocker(const QString& sessionId, int &exitStatus) {
@@ -93,25 +59,8 @@ void fpdunlocker(const QString& sessionId, int &exitStatus) {
         qDebug() << "Identified finger: " << finger;
 
         if (wlrdisplay_status() == 0) {
-            QSharedPointer<QProcess> process(new QProcess());
-
-            QString vibraGood = getVibrationLevel(true);
-            QString unlock = "loginctl unlock-session " + sessionId;
-            QString command = unlock;
-
-            if(!vibraGood.isEmpty()) {
-                command = vibraGood + " && " + command;
-            }
-
-            process->start("bash", QStringList() << "-c" << command);
-
-            QObject::connect(process.data(), QOverload<int, QProcess::ExitStatus>::of(&QProcess::finished),
-                [process, &exitStatus](int exitCode, QProcess::ExitStatus) {
-                    exitStatus = exitCode;
-                });
-
-            // Wait for the process to finish
-            process->waitForFinished();
+            sendFeedback("button-released");
+            unlockSession(sessionId, exitStatus);
         } else {
             exitStatus = 0;
         }
@@ -123,15 +72,7 @@ void fpdunlocker(const QString& sessionId, int &exitStatus) {
         qDebug() << "Error info:" << info;
 
         if (info.contains("FINGER_NOT_RECOGNIZED") && wlrdisplay_status() == 0) {
-            QSharedPointer<QProcess> process(new QProcess());
-            QString vibraBad = getVibrationLevel(false);
-            if(!vibraBad.isEmpty()) {
-                process->start("bash", QStringList() << "-c" << vibraBad);
-
-                // Wait for the process to finish
-                process->waitForFinished();
-            }
-
+            sendFeedback("window-close");
             exitStatus = 1;
         } else if (info.contains("ERROR_CANCELED") && wlrdisplay_status() != 0) {
             exitStatus = 1;
@@ -193,6 +134,7 @@ int main(int argc, char *argv[]) {
         }
 
         if (fgets(sessionId, sizeof(sessionId), fp) != NULL) {
+            sessionId[strcspn(sessionId, "\r\n")] = 0;
             pclose(fp);
             break;
         } else {
