fff97f1c26ccf70d5f175d8092a2b22e3a1e00bb
Bardia Moshiri <bardia@furilabs.com>
2024-12-14 20:15:29 -0500

    fpdlistener: block on IdleHint instead of polling every second
    Signed-off-by: Bardia Moshiri <bardia@furilabs.com>

--- src/fpdlistener.cpp
+++ src/fpdlistener.cpp
@@ -20,10 +20,6 @@ extern "C" int wlrdisplay_status() {
     return result != 0;
 }
 
-extern "C" int delay(double seconds) {
-    return usleep(seconds * 1000000);
-}
-
 static bool isCollectionLocked() {
     const QString service = "org.freedesktop.secrets";
     const QString path = "/org/freedesktop/secrets/collection/login";
@@ -167,85 +163,132 @@ void sendFeedback(const QString &event) {
     }
 }
 
-void fpdunlocker(QString &sessionId, int &exitStatus) {
-    FPDInterface fpdInterface;
-    QEventLoop loop;
-    exitStatus = 0;
+class FpdUnlocker : public QObject {
+    Q_OBJECT
+public:
+    FpdUnlocker(QString sessionId, QObject *parent = nullptr)
+        : QObject(parent), m_sessionId(sessionId), m_inProgress(false) {}
 
-    QObject::connect(&fpdInterface, &FPDInterface::identified, [&](const QString &finger) {
-        qDebug() << "Identified finger:" << finger;
-
-        bool keyring_locked = isCollectionLocked();
-        bool screen_locked = isScreenLocked(sessionId);
-        if (wlrdisplay_status() == 0 && screen_locked && !keyring_locked) {
-            sendFeedback("button-released");
-            exitStatus = unlockSession(sessionId);
-        } else {
-            if (keyring_locked)
-                qDebug() << "Keyring is still locked, discarding fingerprint request";
-            if (!screen_locked)
-                qDebug() << "Screen is unlocked, discarding fingerprint request";
-            if (wlrdisplay_status() != 0)
-                qDebug() << "Display is off, discarding fingerprint request";
-            exitStatus = 0;
+    void startUnlockAttempt() {
+        if (m_inProgress) {
+            qDebug() << "Unlock attempt already in progress, skipping...";
+            return;
         }
+        m_inProgress = true;
+        attemptUnlock();
+    }
 
-        loop.quit();
-    });
+private:
+    void attemptUnlock() {
+        FPDInterface *fpdInterface = new FPDInterface(this);
+        QEventLoop *loop = new QEventLoop(this);
 
-    QObject::connect(&fpdInterface, &FPDInterface::errorInfo, [&](const QString &info) {
-        qDebug() << "Error info:" << info;
+        QObject::connect(fpdInterface, &FPDInterface::identified, this, [=](const QString &finger) {
+            qDebug() << "Identified finger:" << finger;
 
-        if (info.contains("FINGER_NOT_RECOGNIZED") && wlrdisplay_status() == 0) {
-            sendFeedback("window-close");
-            exitStatus = 1;
-            loop.quit();
-        } else if (info.contains("ERROR_CANCELED") && wlrdisplay_status() != 0) {
-            exitStatus = 1;
-            loop.quit();
-        } else if (info.contains("ERROR_CANCELED") && wlrdisplay_status() == 0) {
-            qDebug() << "Fingerprint timed out. Waiting for finger identification again...";
-            fpdInterface.identify();
-        } else {
-            qDebug() << "timed out and reached here";
-            exitStatus = 0;
-            loop.quit();
-        }
-    });
-
-    qDebug() << "Waiting for finger identification...";
-
-    fpdInterface.identify();
-
-    loop.exec();
-}
-
-extern "C" void fpdrunner(const char *initialSessionId) {
-    QString sessionId(initialSessionId);
-    int oldStat = -1;
-
-    while (true) {
-        int dispStat = wlrdisplay_status() == 0 ? 1 : 0;
-        int exitStatus = 0;
-
-        if (oldStat != dispStat) {
-            oldStat = dispStat;
-
-            if (dispStat == 1) {
-                int unlocked = 0;
-                while (unlocked == 0) {
-                    fpdunlocker(sessionId, exitStatus);
-                    if (exitStatus == 0) {
-                        unlocked = 1;
-                    } else {
-                        delay(0.5);
-                    }
-                }
+            bool keyring_locked = isCollectionLocked();
+            bool screen_locked = isScreenLocked(m_sessionId);
+            if (wlrdisplay_status() == 0 && screen_locked && !keyring_locked) {
+                sendFeedback("button-released");
+                unlockSession(m_sessionId);
+                loop->quit();
+            } else {
+                if (keyring_locked)
+                    qDebug() << "Keyring is still locked, discarding fingerprint request";
+                if (!screen_locked)
+                    qDebug() << "Screen is unlocked, discarding fingerprint request";
+                if (wlrdisplay_status() != 0)
+                    qDebug() << "Display is off, discarding fingerprint request";
+                loop->quit();
             }
-        } else {
-            delay(0.2);
+        });
+
+        QObject::connect(fpdInterface, &FPDInterface::errorInfo, this, [=](const QString &info) {
+            qDebug() << "Error info:" << info;
+
+            // Handle errors:
+            // - If finger not recognized and display is on: try again (no quit)
+            // - If canceled and display is off: end (quit)
+            // - If canceled and display is on (timeout): try again (no quit)
+            // - Anything else: end (quit)
+            if (info.contains("FINGER_NOT_RECOGNIZED") && wlrdisplay_status() == 0) {
+                sendFeedback("window-close");
+                qDebug() << "Finger not recognized. Asking again...";
+                fpdInterface->identify();
+            } else if (info.contains("ERROR_CANCELED") && wlrdisplay_status() != 0) {
+                // Display is off, no point in continuing
+                qDebug() << "Operation canceled and display is off. Stopping attempts.";
+                loop->quit();
+            } else if (info.contains("ERROR_CANCELED") && wlrdisplay_status() == 0) {
+                // Display is on, means it timed out, let's try again
+                qDebug() << "Fingerprint timed out. Waiting for finger identification again...";
+                fpdInterface->identify();
+            } else {
+                loop->quit();
+            }
+        });
+
+        qDebug() << "Waiting for finger identification...";
+        fpdInterface->identify();
+
+        loop->exec();
+
+        m_inProgress = false;
+        fpdInterface->deleteLater();
+        loop->deleteLater();
+    }
+
+    QString m_sessionId;
+    bool m_inProgress;
+};
+
+class IdleHintListener : public QObject {
+    Q_OBJECT
+public:
+    IdleHintListener(const QString &sessionId, QObject *parent = nullptr)
+        : QObject(parent), m_sessionId(sessionId), m_unlocker(new FpdUnlocker(sessionId, this)) {}
+
+public slots:
+    void onPropertiesChanged(const QString &interface_name, const QVariantMap &changed_properties, const QStringList &invalidated_properties) {
+        Q_UNUSED(invalidated_properties)
+        if (interface_name == "org.freedesktop.login1.Session" && changed_properties.contains("IdleHint")) {
+            bool idleHint = changed_properties.value("IdleHint").toBool();
+            qDebug() << "IdleHint changed:" << idleHint;
+
+            if (!idleHint)
+                m_unlocker->startUnlockAttempt();
         }
     }
+
+private:
+    QString m_sessionId;
+    FpdUnlocker *m_unlocker;
+};
+
+void listenForIdleHint(const QString &sessionId) {
+    QString sessionPath = QString("/org/freedesktop/login1/session/%1").arg(sessionId);
+
+    IdleHintListener *listener = new IdleHintListener(sessionId);
+
+    bool connected = false;
+    while (!connected) {
+        connected = QDBusConnection::systemBus().connect(
+            "org.freedesktop.login1",
+            sessionPath,
+            "org.freedesktop.DBus.Properties",
+            "PropertiesChanged",
+            "sa{sv}as",
+            listener,
+            SLOT(onPropertiesChanged(QString,QVariantMap,QStringList))
+        );
+
+        if (!connected) {
+            qWarning() << "Failed to connect to PropertiesChanged signal for IdleHint. Retrying in 1 second...";
+            QThread::sleep(1);
+        }
+    }
+
+    qDebug() << "Listening for IdleHint changes...";
 }
 
 int main(int argc, char *argv[]) {
@@ -255,10 +298,9 @@ int main(int argc, char *argv[]) {
 
     QString sessionId = getSessionId();
 
-    QThread *mainLoopThread = QThread::create([=]() { fpdrunner(sessionId.toUtf8().constData()); });
-    QObject::connect(mainLoopThread, &QThread::finished, mainLoopThread, &QThread::deleteLater);
-
-    mainLoopThread->start();
+    listenForIdleHint(sessionId);
 
     return app.exec();
 }
+
+#include "fpdlistener.moc"
